5ed712a5d5bbc29952945b6225015c75dbb427b3
Brenden <brenden.black@gov.bc.ca>
2018-03-28 06:19:22 -0700

    adding debug settings

--- src/SpringfieldRecMvc/Program.cs
+++ src/SpringfieldRecMvc/Program.cs
@@ -19,7 +19,12 @@ namespace SpringfieldRecMvc
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseSetting("detailedErrors", "true")
+                .UseIISIntegration()
                 .UseStartup<Startup>()
+                .CaptureStartupErrors(true)
                 .Build();
     }
 }
